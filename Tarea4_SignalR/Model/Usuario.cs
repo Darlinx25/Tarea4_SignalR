b668bebd90909b3764c5b8a7a36b60bfba33b14c
Darlinx25 <fsalaberry505@gmail.com>
2026-09-19 12:43:43 -0300

    hub y model base

---
+++ Tarea4_SignalR/Model/Usuario.cs
@@ -0,0 +1,27 @@
+namespace Tarea4_SignalR.Model
+{
+public class Usuario
+{
+    public String Email { get; set; }
+    public String Password { get; set; }
+
+
+    public Usuario(String email, String pass) {
+        this.Email = email;
+        this.Password = pass;
+    }
+
+    public Boolean EsUsuarioValido()
+    {
+        //TODO implementar lógica de autenticacion
+        return true;
+    }
+
+    public Boolean NecesitarVerificacion()
+    {
+        //TODO implmentar lógica de verificación
+        return true; 
+    }
+
+}
+}
